e5ddda9236330926561a170bdfe1d5ef1b255fc2
Dani <danielito92000hotmail.com@gmail.com>
2020-06-16 08:44:26 -0500

    empezando las vistas

---
+++ features/step_definitions/cuenta_steps.rb
@@ -0,0 +1,14 @@
+Given('visito a la pagina de transacciones') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('lleno en el campo {string} con {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  When('presiono el boton de {string}') do |string|
+  end
+  
+  Then('me deberia mostrar {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
